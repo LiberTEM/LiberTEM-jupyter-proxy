b2e9e6f07601bb4c4d476f7babde2069b4977c9d
Alexander Clausen <alex@gc-web.de>
2021-08-17 20:08:32 +0200

    Add token authentication
    Note that this depends on
    https://github.com/jupyterhub/jupyter-server-proxy/pull/287 to be
    merged, otherwise websocket connections will fail.

--- libertem_jupyter_proxy/__init__.py
+++ libertem_jupyter_proxy/__init__.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 import shutil
+import secrets
+from tempfile import mkdtemp
 
 
 def _get_libertem_path():
@@ -20,22 +22,50 @@ def _get_libertem_path():
     raise FileNotFoundError("Can not find libertem-server in configuration or PATH")
 
 
-def _get_libertem_cmd(port):
-    path = _get_libertem_path()
+def make_get_libertem_cmd(token_path):
+    def _get_libertem_cmd(port):
+        path = _get_libertem_path()
 
-    cmd = [
-        path,
-        "--no-browser",
-        "--port=" + str(port),
-    ]
+        cmd = [
+            path,
+            "--no-browser",
+            "--port=" + str(port),
+            "--token-path=" + str(token_path),
+        ]
 
-    return cmd
+        return cmd
+    return _get_libertem_cmd
+
+
+def make_token():
+    return secrets.token_urlsafe(32)
+
+
+def store_token(token):
+    """
+    Make a temporary directory with access limited to the current user,
+    and store the token in a new file in that directory.
+
+    Returns the path to the token file.
+    """
+    # mkdtemp makes sure the directory is only readable by the current user:
+    token_dir = mkdtemp(prefix="libertem")
+    token_path = os.path.join(token_dir, 'libertem-server-token')
+    with open(token_path, 'w') as f:
+        f.write(token)
+    return token_path
 
 
 def setup_libertem():
+    token = make_token()
+    token_path = store_token(token)
+
     return {
-        "command": _get_libertem_cmd,
+        "command": make_get_libertem_cmd(token_path),
         "timeout": 20,
+        "request_headers_override": {
+            "X-Api-Key": token,
+        },
         "new_browser_tab": True,
         "launcher_entry": {
             "title": "LiberTEM",
