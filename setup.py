13580a858b6a11edd1c45453ddf29904761db9d3
Alexander Clausen <alex@gc-web.de>
2022-02-22 14:25:20 +0100

    Bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf8") as f:
 
 setuptools.setup(
     name="libertem-jupyter-proxy",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/LiberTEM/LiberTEM-jupyter-proxy",
     author="",
     license="BSD",
